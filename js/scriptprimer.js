6641c92f248d82d7fa4dcb30e096a3a8eda5d5e0
gonzacat22 <gonzalodurante22@hotmail.com>
2022-07-13 22:20:26 -0300

    Simulador funcionando

--- js/scriptprimer.js
+++ js/scriptprimer.js
@@ -2,37 +2,34 @@ let nombre = prompt('Ingresa tu Nombre');
 let apellido = prompt('Ingresa tu Apellido');
 let opcionCuota = prompt('Elegi Cuantas cuotas vas a pagar: \n1- 1 Mes. \n2- 2 Meses. \n3- 3 Meses. \n4- Semestre. \n Presiona X para Salir');
 
-/* if (opcionCuota==1){
-    opcionCuota = 2200;
- } else if(opcionCuota==2){
-   opcionCuota = (2200*2)*.90
- } else if(opcionCuota==3){
-    opcionCuota = (2200*2)*.85
-   //else if(opcionCuota==4){
-       // opcionCuota = (2200*2)*.75
-    //}
- 
- }    */
 
-while(opcionCuota!='X'|| opcion!='x'){
+
+while(opcionCuota!='X'&& opcionCuota!='x'){
     switch (opcionCuota) {
         case '1':
             opcionCuota = 2200;
+            alert('El monto final a pagar es de: '+opcionCuota)
             break;
         case '2':
             opcionCuota = (2200*2)*.90;
+            alert('El monto final a pagar es de: '+opcionCuota)
             break;
         case '3':
-            opcionCuota = (2200*2)*.85;
+            opcionCuota = (2200*3)*.85;
+            alert('El monto final a pagar es de: '+opcionCuota)
             break; 
         case '4':
-            opcionCuota = (2200*2)*.75;
-            break;   
+            opcionCuota = (2200*6)*.75;
+            alert('El monto final a pagar es de: '+opcionCuota)
+            break; 
+        
     
         default:
             alert('Elegiste una opcion no valida')
             break;
+ 
     }
+    opcionCuota = prompt('Elegi Cuantas cuotas vas a pagar: \n1- 1 Mes. \n2- 2 Meses. \n3- 3 Meses. \n4- Semestre. \n Presiona X para Salir');   
 }
 
 
