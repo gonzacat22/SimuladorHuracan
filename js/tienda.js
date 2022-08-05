38ceac5627dcb4db0491d85a0463b4217d50c77c
gonzacat22 <gonzalodurante22@hotmail.com>
2022-08-05 09:19:17 -0300

    agregandolibrerias

--- js/tienda.js
+++ js/tienda.js
@@ -1,240 +1,244 @@
-
 //constantes para socios formulario
 
-const socios = [{
-  nombre: 'Gonzalo',
-  mail: 'gonzalodurante294@gmail.com',
-  password: 'gonza22'
-},
-{
-  nombre: 'Juan Ignacio',
-  mail: 'juanignacio294@gmail.com',
-  password: 'juan22'
-},
-{
-  nombre: 'Pedro',
-  mail: 'pedro294@gmail.com',
-  password: 'pedro22'
-}]
+const socios = [
+  {
+    id: 1,
+    name: "Gonzalo",
+    mail: "gonzalodurante294@gmail.com",
+    password: "gonza22",
+  },
+  {
+    id: 2,
+    name: "Juan Ignacio",
+    mail: "juanignacio294@gmail.com",
+    password: "juan22",
+  },
+  {
+    id: 3,
+    name: "Pedro",
+    mail: "pedro294@gmail.com",
+    password: "pedro22",
+  },
+];
 
 //productos para no hardcodearlos
-const indumentaria = [{
-  tituloItem: 'Cuota Social',
-  itemPrice: 2200,
-  itemImage: './img/huracanCuotaSocial.jpg',
-  btnAgregaCarrito: 'Añadir Carrito'
-}, {
-  tituloItem: 'Camiseta de Juego',
-  itemPrice: 1600,
-  itemImage: './img/huracanCamisetaJuego.jpg',
-  btnAgregaCarrito: 'Añadir Carrito'
-}, {
-  tituloItem: 'Pantalon de Juego',
-  itemPrice: 1200,
-  itemImage: './img/huracanPantalonJuego.jpg',
-  btnAgregaCarrito: 'Añadir Carrito'
-}, {
-  tituloItem: 'Medias de juego',
-  itemPrice: 900,
-  itemImage: './img/huracanMediasJuego.jpg',
-  btnAgregaCarrito: 'Añadir Carrito'
-}, {
-  tituloItem: 'Campera',
-  itemPrice: 2500,
-  itemImage: './img/huracanCampera.jpg',
-  btnAgregaCarrito: 'Añadir Carrito'
-}, {
-  tituloItem: 'Pantalon Largo',
-  itemPrice: 2400,
-  itemImage: './img/huracanPantalonLargo.jpg',
-  btnAgregaCarrito: 'Añadir Carrito'
-}]
+const indumentaria = [
+  {
+    id: 01,
+    tituloItem: "Cuota Social",
+    itemPrice: 2200,
+    itemImage: "./img/huracanCuotaSocial.jpg",
+    btnAgregaCarrito: "Añadir Carrito",
+  },
+  {
+    id: 02,
+    tituloItem: "Camiseta de Juego",
+    itemPrice: 1600,
+    itemImage: "./img/huracanCamisetaJuego.jpg",
+    btnAgregaCarrito: "Añadir Carrito",
+  },
+  {
+    id: 03,
+    tituloItem: "Pantalon de Juego",
+    itemPrice: 1200,
+    itemImage: "./img/huracanPantalonJuego.jpg",
+    btnAgregaCarrito: "Añadir Carrito",
+  },
+  {
+    id: 04,
+    tituloItem: "Medias de juego",
+    itemPrice: 900,
+    itemImage: "./img/huracanMediasJuego.jpg",
+    btnAgregaCarrito: "Añadir Carrito",
+  },
+  {
+    id: 05,
+    tituloItem: "Campera",
+    itemPrice: 2500,
+    itemImage: "./img/huracanCampera.jpg",
+    btnAgregaCarrito: "Añadir Carrito",
+  },
+  {
+    id: 06,
+    tituloItem: "Pantalon Largo",
+    itemPrice: 2400,
+    itemImage: "./img/huracanPantalonLargo.jpg",
+    btnAgregaCarrito: "Añadir Carrito",
+  },
+];
 // guardo el formulario
-const formulario = document.getElementById('mostrandoInfoGuardada'); 
+const formulario = document.getElementById("mostrandoInfoFormulario");
 // botón para mostrar informacion
-const botonMostrar = document.getElementById('mostrandoInfoGuardada'); 
+const botonMostrar = document.getElementById("mostrandoInfoGuardada");
 // botón para borrar informacion
-const botonBorrar = document.getElementById('borrarInfoGuardada'); 
+const botonBorrar = document.getElementById("borrarInfoGuardada");
 // guardo el elemento que va a contener informacion de los socios
-const infoContenedor = document.getElementById('infoContenedo'); 
+const infoContenedor = document.getElementById("infoContenedo");
 
 // paro el comportamiento del formulario
 const guardarInfo = (e) => {
-  e.preventDefault(); 
+  e.preventDefault();
 
   // guardo los valores en variables no estoy seguro las ultimas 2
-  const nombre = document.getElementById('nombre').value;
-  const email = document.getElementById('email').value;
-  const password = document.getElementById('password').checked;
-  const esSocio = document.getElementById('esSocio').checked;
-  const noSocio = document.getElementById('noSocio').checked;
+  const id = document.getElementById("id").value;
+  const name = document.getElementById("name").value;
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+  const esSocio = document.getElementById("esSocio").checked;
 
   // genero un objeto con esas variables
   // const socios = {
-  //   nombre:nombre,
+  //   name:name,
   //   email:email,
   //   password:password,
   //   esSocio:esSocio,
-    
+
   // };
   // busco en el storage y lo guardo en una constante
-  const listaSocios = localStorage.getItem('listaUsuarios'); 
+  const listaSocios = localStorage.getItem("listaSocios");
   // parseo la info, si no existe que guarde array vacio
-  const listaSociosParseada = JSON.parse(listaUsuarios) || []; 
+  const listaSociosParseada = JSON.parse(listaSocios) || [];
   // hago un push para agregar esta nueva info al array
-  listaSociosParseada.push(socios); 
+  listaSociosParseada.push(socios);
   // Lo convierto a string para guardarlo en storage
-  localStorage.setItem('listaUsuarios', JSON.stringify(listaSociosParseada)); 
+  localStorage.setItem("listaSocios", JSON.stringify(listaSociosParseada));
 
   // reseteo los datos que cargaron
   formulario.reset();
-  // ejecuto función para mostrar cuando se agrega uno nuevo 
+  // ejecuto función para mostrar cuando se agrega uno nuevo
   mostrarInfo(); // ejecuto función para mostrar cuando se agrega uno nuevo
 };
 
 const mostrarInfo = () => {
   // me aseguro que no tenga nada al momento de mostrar la info
-  infoContenedor.innerHTML = ''; 
+  infoContenedor.innerHTML = "";
   // guardo en una variable lo que tengo en el storage
-  const infoDesdeStorage = localStorage.getItem('listaUsuarios'); 
+  const infoDesdeStorage = localStorage.getItem("listaSocios");
 
-  
-  
-  //Aca me re perdi Gonza
-  
-  
   // si existe la info, que la muestre. Sino que muestre un mensaje del html
-  if (infoDesdeStorage) { 
+  if (infoDesdeStorage) {
     // lo convierto a objeto para poder operar con JS
-    const infoParseada = JSON.parse(infoDesdeStorage); 
+    const infoParseada = JSON.parse(infoDesdeStorage);
     // hago un foreach() para iterar el array que obtengo del storage, para mostrar la info de cada usuario
-    infoParseada.forEach((user, indexDelUser) => { // el forEach devuelve el elemento, junto con el índice del mismo dentro del array 
+    infoParseada.forEach((user, indexDelUser) => {
+      // el forEach devuelve el elemento, junto con el índice del mismo dentro del array
       // creo una nueva tarjeta (contenedor de la info del usuario)
-      const nuevaCard = document.createElement('div');
+      const nuevoUsuario = document.createElement("div");
       // agrego una clase, para el estilo
-      nuevaCard.className = "userCard";
+      nuevoUsuario.className = "usuarioNuevo";
       // le agrego el contenido de la tarjeta, utilizo backticks para facilitar
-      nuevaCard.innerHTML = ` 
-        <h3>${user.nombre}</h3>
+      nuevoUsuario.innerHTML = ` 
+        <h3>${user.name}</h3>
         <p>${user.email}</p>
         <p>${user.password}</p> 
-        <p>${user.esSocio ? 'Es Socio' : 'No Es Socio'}</p>
+        <p>${user.esSocio ? "Es Socio" : "No Es Socio"}</p>
         
-        <button type="button" onclick="borrarItemEspecifico(${indexDelUser})">Borrar Item</button>
+        // <button type="button" onclick="borrarItemEspecifico(${indexDelUser})">Borrar Item</button> //aca teniamos que agregar algo dentro del $
         
-      `
+      `;
       // aqui arriba no se si sirve traer el pass
 
       // Le agrego al botón "borrar item" mediante el onclick, la función para borrar el item. Le paso como parámetro el índice del elemento actual.
       // Con "user.programador" estoy haciendo un if ternario: Que muestre 'programador' si es true, y que muestre 'no es programador' si es false.
       // Por último, agrego la tarjeta al contenedor de tarjetas.
-      infoContenedor.append(nuevaCard);
-    })  
+      infoContenedor.append(nuevoUsuario);
+    });
   } else {
-    infoContenedor.innerHTML = '<h2>No hay info para mostrar</h2>'; // mensaje para cuando no hay info
+    infoContenedor.innerHTML = "<h2>No hay info para mostrar</h2>"; // mensaje para cuando no hay info
   }
-}
+};
 
 const borrarListaStorage = () => {
-  localStorage.removeItem('listaUsuarios'); // elimino el item donde guardaba la info en el storage
+  localStorage.removeItem("listaSocios"); // elimino el item donde guardaba la info en el storage
 
   mostrarInfo(); // vuelvo a ejecutar para mostrar actualización
-}
+};
 
 const borrarItemEspecifico = (indiceDelUsuario) => {
-  const infoDesdeStorage = localStorage.getItem('listaUsuarios'); // guardo en una variable lo que tengo en el storage
+  const infoDesdeStorage = localStorage.getItem("listaSocios"); // guardo en una variable lo que tengo en el storage
   const infoParseada = JSON.parse(infoDesdeStorage); // lo convierto a objeto para poder operar con JS
 
-  if (infoParseada.length === 1) { // si hay un solo elemento dentro del array, borra todo el item del storage
+  if (infoParseada.length === 1) {
+    // si hay un solo elemento dentro del array, borra todo el item del storage
     borrarListaStorage();
-  } else { // si hay más de un elemento guardado, ejecuto lo siguiente
+  } else {
+    // si hay más de un elemento guardado, ejecuto lo siguiente
     infoParseada.splice(indiceDelUsuario, 1); // ejecuto un splice para eliminar desde el indice, un espacio
-    localStorage.setItem('listaUsuarios', JSON.stringify(infoParseada)) // guardo el array actualizado
-    mostrarInfo(); // ejecuto función para mostrar la info actualizada  
+    localStorage.setItem("listaSocios", JSON.stringify(infoParseada)); // guardo el array actualizado
+    mostrarInfo(); // ejecuto función para mostrar la info actualizada
   }
-}
+};
 
 // ---------------------------- Seteo eventos -----------------------------
-formulario.onsubmit = (e) => guardarInfo(e);
-botonMostrar.onclick = () => mostrarInfo();
-botonBorrar.onclick = () => borrarListaStorage();
-
-
-
-
-
-
-
-
-
+// formulario.onsubmit = (e) => guardarInfo(e);
+// botonMostrar.onclick = () => mostrarInfo();
+// botonBorrar.onclick = () => borrarListaStorage();
 
 //Traje el contenedor de la tienda mediante el id lo entendi
-const contentIndumentaria = document.querySelector('#contentIndumentaria')
+const contentIndumentaria = document.querySelector("#contentIndumentaria");
 //aca itero el array de indumentaria
-indumentaria.forEach(item => {
+indumentaria.forEach((item) => {
   //entendi que debia solo traer un div y luego dinamicamente js agrega todos
   contentIndumentaria.innerHTML += `
                       <div class="col-12 col-md-6">
                         <div class="item shadow mb-4">
                             <h3 class="item-title">${item.tituloItem}</h3>
-                            <img class="item-image" src=${item.itemImage}>
-
                             <div class="item-details">
                                 <h4 class="item-price">$${item.itemPrice}</h4>
-                                <button class="item-button btn btn-primary agregaCarrito">AÑADIR AL CARRITO</button>
+                                
+                            
+                            <img class="item-image" src=${item.itemImage}>
+                            <button class="item-button btn btn-primary agregaCarrito">AÑADIR AL CARRITO</button>
+
                             </div>
                         </div>
-                      </div>`
-
-})
+                      </div>`;
+});
 //los botones "agregar carrito"
-const traeTodosLosBotones = document.querySelectorAll('.agregaCarrito');
+const traeTodosLosBotones = document.querySelectorAll(".agregaCarrito");
 traeTodosLosBotones.forEach((agregaCarritoBoton) => {
-  agregaCarritoBoton.addEventListener('click', agregaCarritoClickeando);
+  agregaCarritoBoton.addEventListener("click", agregaCarritoClickeando);
 });
 //boton de comprar abajo
-const botonComprar = document.querySelector('.botonComprar');
-botonComprar.addEventListener('click', botonComprarClick);
+const botonComprar = document.querySelector(".botonComprar");
+botonComprar.addEventListener("click", botonComprarClick);
 //esta lleva los items al carrito
-const contenedorDeItems = document.querySelector(
-  '.contenedorDeItems'
-);
+const contenedorDeItems = document.querySelector(".contenedorDeItems");
 
 //aca los agrega al carrito
 
 function agregaCarritoClickeando(event) {
   const boton = event.target;
-  const item = boton.closest('.item');
+  const item = boton.closest(".item");
 
-  const tituloItem = item.querySelector('.item-title').textContent;
-  const itemPrice = item.querySelector('.item-price').textContent;
-  const itemImage = item.querySelector('.item-image').src;
+  const tituloItem = item.querySelector(".item-title").textContent;
+  const itemPrice = item.querySelector(".item-price").textContent;
+  const itemImage = item.querySelector(".item-image").src;
 
   addItemToShoppingCart(tituloItem, itemPrice, itemImage);
 }
 
 function addItemToShoppingCart(itemTitle, itemPrice, itemImage) {
   const elementsTitle = contenedorDeItems.getElementsByClassName(
-    'shoppingCartItemTitle'
+    "shoppingCartItemTitle"
   );
   for (let i = 0; i < elementsTitle.length; i++) {
     if (elementsTitle[i].innerText === itemTitle) {
       let elementQuantity = elementsTitle[
         i
       ].parentElement.parentElement.parentElement.querySelector(
-        '.shoppingCartItemQuantity'
+        ".shoppingCartItemQuantity"
       );
       elementQuantity.value++;
-      $('.toast').toast('show');
+      $(".toast").toast("show");
       updateShoppingCartTotal();
       return;
     }
   }
 
-const shoppingCartRow = document.createElement('div');
+  const shoppingCartRow = document.createElement("div");
 
-
-const shoppingCartContent = `
+  const shoppingCartContent = `
 <div class="row shoppingCartItem">
       <div class="col-6">
           <div class="shopping-cart-item d-flex align-items-center h-100 border-bottom pb-2 pt-3">
@@ -256,35 +260,35 @@ const shoppingCartContent = `
           </div>
       </div>
   </div>`;
-shoppingCartRow.innerHTML = shoppingCartContent;//limpio el html
-contenedorDeItems.append(shoppingCartRow);
+  shoppingCartRow.innerHTML = shoppingCartContent; //limpio el html
+  contenedorDeItems.append(shoppingCartRow);
 
-shoppingCartRow//borrar el carrito
-  .querySelector('.buttonDelete')
-  .addEventListener('click', removeShoppingCartItem);
+  shoppingCartRow //borrar el carrito
+    .querySelector(".buttonDelete")
+    .addEventListener("click", removeShoppingCartItem);
 
-shoppingCartRow
-  .querySelector('.shoppingCartItemQuantity')
-  .addEventListener('change', quantityChanged);
+  shoppingCartRow
+    .querySelector(".shoppingCartItemQuantity")
+    .addEventListener("change", quantityChanged);
 
-updateShoppingCartTotal();
+  updateShoppingCartTotal();
 }
 
 function updateShoppingCartTotal() {
   let total = 0;
-  const shoppingCartTotal = document.querySelector('.shoppingCartTotal');
+  const shoppingCartTotal = document.querySelector(".shoppingCartTotal");
 
-  const shoppingCartItems = document.querySelectorAll('.shoppingCartItem');
+  const shoppingCartItems = document.querySelectorAll(".shoppingCartItem");
 
   shoppingCartItems.forEach((shoppingCartItem) => {
     const shoppingCartItemPriceElement = shoppingCartItem.querySelector(
-      '.shoppingCartItemPrice'
+      ".shoppingCartItemPrice"
     );
     const shoppingCartItemPrice = Number(
-      shoppingCartItemPriceElement.textContent.replace('$', '')
+      shoppingCartItemPriceElement.textContent.replace("$", "")
     );
     const shoppingCartItemQuantityElement = shoppingCartItem.querySelector(
-      '.shoppingCartItemQuantity'
+      ".shoppingCartItemQuantity"
     );
     const shoppingCartItemQuantity = Number(
       shoppingCartItemQuantityElement.value
@@ -296,7 +300,7 @@ function updateShoppingCartTotal() {
 
 function removeShoppingCartItem(event) {
   const botonClick = event.target;
-  botonClick.closest('.shoppingCartItem').remove();
+  botonClick.closest(".shoppingCartItem").remove();
   updateShoppingCartTotal();
 }
 
@@ -307,7 +311,16 @@ function quantityChanged(event) {
 }
 
 function botonComprarClick() {
-  contenedorDeItems.innerHTML = '';
+  contenedorDeItems.innerHTML = "";
   updateShoppingCartTotal();
-}
+  Swal.fire({
+    title: 'Compraste en la tienda Luminosa',
+    text: 'Retira por la sede',
+    icon: 'info',
+    iconColor: '#81f40e',
+    confirmButtonText: 'Gracias',
+    position: 'top-center'
+})
 
+  
+}
